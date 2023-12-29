9b1fce8c8f9d3e0d687497435f928eea827268cc
Julian de Ruiter <julianderuiter@godatadriven.com>
2023-12-29 11:00:25 +0100

    Add Docker socket proxy, add Airflow connections for MLflow/Minio.

--- chapterml/dags/01_docker.py
+++ chapterml/dags/01_docker.py
@@ -12,29 +12,32 @@ with DAG(
     schedule_interval="@daily",
 ) as dag:
 
+    docker_url = "tcp://docker-socket-proxy:2375"
+
     environment= {
-        "MLFLOW_TRACKING_URI": "http://mlflow:9002",
-        "MLFLOW_S3_ENDPOINT_URL": "http://minio:9000",
-        "AWS_ENDPOINT_URL_S3": "http://minio:9000",
+        "MLFLOW_TRACKING_URI": "{{ conn.mlflow.get_uri() }}",
+        "MLFLOW_S3_ENDPOINT_URL": "{{ conn.minio.extra_dejson.get('endpoint_url') }}",
+        "AWS_ENDPOINT_URL_S3": "{{ conn.minio.extra_dejson.get('endpoint_url') }}",
         "AWS_ACCESS_KEY_ID": "{{ conn.minio.login }}",
         "AWS_SECRET_ACCESS_KEY": "{{ conn.minio.password }}"
     }
 
     fetch_dataset = DockerOperator(
         task_id="fetch_dataset",
+        docker_url=docker_url,
         image="jrderuiter/fashion-model",
         command=[
             "fetch",
             "s3://data/{{ds}}/train",
             "s3://data/{{ds}}/test",
         ],
-        network_mode="chapterml_airflow",
+        network_mode="chapterml_default",
         environment=environment
     )
 
-
     train_model = DockerOperator(
         task_id="train_model",
+        docker_url=docker_url,
         image="jrderuiter/fashion-model",
         command=[
             "train",
@@ -42,19 +45,20 @@ with DAG(
             "--epochs",
             "5"
         ],
-        network_mode="chapterml_airflow",
+        network_mode="chapterml_default",
         environment=environment
     )
 
     evaluate_model = DockerOperator(
         task_id="evaluate_model",
+        docker_url=docker_url,
         image="jrderuiter/fashion-model",
         command=[
             "evaluate",
             "s3://data/{{ds}}/test",
             "{{ task_instance.xcom_pull(task_ids='train_model', key='return_value') }}"
         ],
-        network_mode="chapterml_airflow",
+        network_mode="chapterml_default",
         environment=environment
     )
 
