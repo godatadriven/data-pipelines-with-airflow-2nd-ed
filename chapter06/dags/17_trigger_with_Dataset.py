4c390ceed40889b0a8d6bbb3c5e5d53905f6a7c7
Ismael Cabral <ismaelcv@gmail.com>
2024-06-04 09:15:31 +0200

    dataset dag updated

--- chapter06/dags/17_trigger_with_Dataset.py
+++ chapter06/dags/17_trigger_with_Dataset.py
@@ -9,22 +9,23 @@ for dag_id in range(1, 4):
     with DAG(
         dag_id=f"17_etl_{dag_id}",
         start_date=pendulum.today("UTC").add(days=-3),
-        schedule="0 16 * * *",
+        schedule=None,
     ):
         etl =  EmptyOperator(
             task_id="save_data",
-            outlets=[Dataset(f"/data/supermarket_{dag_id}") ],
+            outlets=[Dataset(f"/data/supermarket_{dag_id}.csv") ],
         )
 
-dag1, dag2, dag3 = [ 
-    Dataset(f"/data/supermarket_{dag_id}") for dag_id in range(1, 4) 
-    ]
 
 
 with DAG(
     dag_id="17_report",
     start_date=pendulum.today("UTC").add(days=-3),
-    schedule=((dag1 | dag2) & dag3),
+    schedule=(
+        (
+            Dataset(f"/data/supermarket_1.csv") | Dataset(f"/data/supermarket_2.csv")
+        ) 
+        & Dataset(f"/data/supermarket_3.csv")),
 ):
-    report = EmptyOperator(task_id="compute_differences")
+     EmptyOperator(task_id="create_report")
 
