df98ce17cd00d9f4272373912196b269087a86d5
Daniel van der Ende <daniel.vanderende@gmail.com>
2024-08-21 20:20:34 +0200

    Fix linting

--- chapter09/dags/testme.py
+++ chapter09/dags/testme.py
@@ -3,11 +3,11 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
-dag = DAG(
+with DAG(
     dag_id="testme",
     start_date=pendulum.today("UTC").add(days=-3),
-    schedule=None)
-
-t1 = EmptyOperator(task_id="test", dag=dag)
-for tasknr in range(5):
-    BashOperator(task_id="test2", bash_command=f"echo '{tasknr}'", dag=dag) >> t1
+    schedule=None,
+):
+    t1 = EmptyOperator(task_id="test")
+    for tasknr in range(5):
+        BashOperator(task_id="test2", bash_command=f"echo '{tasknr}'") >> t1
