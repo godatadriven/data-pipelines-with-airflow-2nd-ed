5d27e610ef26991a9404462d5f37bbb2bedfd072
Daniel van der Ende <daniel.vanderende@gmail.com>
2024-10-29 10:10:00 +0100

    Add final DAGs

--- chapter11/dags/06_dynamic_task_mapping.py
+++ chapter11/dags/06_dynamic_task_mapping.py
@@ -14,12 +14,15 @@ def _print_rating(rating):
     print(f"New rating for Movie: {rating["movie"]}. Rating: {rating["rating"]}")
 
 
-with DAG(dag_id="06_dynamic_task_mapping",start_date=pendulum.today("UTC").add(days=-5), schedule="@daily") as dag:
+with DAG(dag_id="06_dynamic_task_mapping",start_date=pendulum.today("UTC").add(days=-5), schedule="@daily"):
     fetch_ratings = PythonOperator(
         task_id="fetch_ratings",
         python_callable=_fetch_ratings
     )
 
-    print_rating = PythonOperator.partial(task_id="print_rating", python_callable=_print_rating).expand(op_args=fetch_ratings.output)
+    print_rating = PythonOperator.partial(
+        task_id="print_rating",
+        python_callable=_print_rating
+    ).expand(op_args=fetch_ratings.output)
 
     fetch_ratings >> print_rating
--- chapter11/dags/07_dynamic_task_mapping_taskflow.py
+++ chapter11/dags/07_dynamic_task_mapping_taskflow.py
@@ -6,15 +6,14 @@ from airflow.decorators import task
 
 @task
 def fetch_ratings():
-    "Retrieve the latest ratings from the movie reviews API. The number of reviews varies per request"
     data = requests.get("http://movie-reviews:8081/reviews/latest")
-    return data.json()
+    return data.json() #A
 
 
 @task
-def print_rating(rating):
+def print_rating(rating): #B
     print(f"New rating for Movie: {rating["movie"]}. Rating: {rating["rating"]}")
 
 
 with DAG(dag_id="07_dynamic_task_mapping_taskflow",start_date=pendulum.today("UTC").add(days=-5), schedule="@daily") as dag:
-    print_rating.expand(rating=fetch_ratings())
+    print_rating.expand(rating=fetch_ratings()) #C
